2b5e86e2ac0b248a0d96233a1fd890332586259b
Artur Teben <teben.artur99@gmail.com>
2020-07-27 21:33:08 +0300

    Create obj app by class Application and call method runApplication

---
+++ src/com/java/streams/Main.java
@@ -0,0 +1,11 @@
+package com.java.streams;
+
+import com.java.streams.executor.Application;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Application app = new Application();
+        app.runApplication();
+    }
+}
