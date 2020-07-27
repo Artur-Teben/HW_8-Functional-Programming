f05f2b791d7170ff2b1e60599d7530f5e871c80e
Artur Teben <teben.artur99@gmail.com>
2020-07-27 21:36:35 +0300

    Implement class User with fields name, age, city; add constructor, getters; override toString, equals, hashCode

---
+++ src/com/java/streams/user/User.java
@@ -0,0 +1,51 @@
+package com.java.streams.user;
+
+import java.util.Objects;
+
+public class User {
+    private final String name;
+    private final int age;
+    private final String city;
+
+    public User(String name, int age, String city) {
+        this.name = name;
+        this.age = age;
+        this.city = city;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", city='" + city + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(city, user.city);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, city);
+    }
+}
